727b10b0d4423338c2e8935f7db15161fccd6262
flackdl <flackattack@gmail.com>
2020-04-19 17:40:37 -0500

    scrape images, ingest (wip)

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,33 @@
+from django.contrib.postgres import fields
+from django.contrib.postgres.indexes import GinIndex
+from django.contrib.postgres.search import SearchVectorField
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    total_time = models.IntegerField()  # minutes
+    servings = models.IntegerField()
+    rating = models.IntegerField(null=True, blank=True)
+    description = models.TextField()
+    ingredients = fields.ArrayField(base_field=models.CharField(max_length=1500))
+    search_vector = SearchVectorField(null=True)  # postgres search vector populated after creation
+
+    class Meta:
+        indexes = [
+            GinIndex(fields=['search_vector']),
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Cuisine(models.Model):
+    name = models.CharField(max_length=100)
+    recipes = models.ManyToManyField(Recipe)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    recipes = models.ManyToManyField(Recipe)
