736fae0ecdf6bb0643495175fc8ce94d9fe71fb3
flackdl <flackattack@gmail.com>
2020-04-21 16:08:53 -0500

    cache scrape results to allow running multiple times, use their unique slug, add rating count

--- recipes/models.py
+++ recipes/models.py
@@ -7,12 +7,14 @@ from django.db.models import Index
 
 class Recipe(models.Model):
     name = models.CharField(max_length=500)
-    slug = models.SlugField(max_length=200)
+    # TODO - pointless if not unique (use their slug vs generating one and can link correctly)
+    slug = models.SlugField(max_length=200, unique=True)
     image_path = models.CharField(max_length=210, null=True, blank=True)
     description = models.TextField()
     total_time = models.IntegerField(null=True, blank=True)  # minutes
     servings = models.CharField(max_length=100)
-    rating = models.IntegerField(null=True, blank=True)
+    rating_value = models.IntegerField(null=True, blank=True)
+    rating_count = models.IntegerField(null=True, blank=True)
     ingredients = fields.ArrayField(base_field=models.CharField(max_length=1500))
     instructions = fields.ArrayField(base_field=models.CharField(max_length=3000))
     categories = models.ManyToManyField('Category')
@@ -24,7 +26,8 @@ class Recipe(models.Model):
         indexes = [
             GinIndex(fields=['search_vector']),
             Index(fields=['slug']),
-            Index(fields=['rating']),
+            Index(fields=['rating_value']),
+            Index(fields=['rating_count']),
         ]
 
     def __str__(self):
