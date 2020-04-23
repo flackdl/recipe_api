d78fcddd41908862bff07a4c92fddd869ebddf12
flackdl <flackattack@gmail.com>
2020-04-23 18:54:06 -0500

    cache and compress results

--- recipes/api/viewsets.py
+++ recipes/api/viewsets.py
@@ -1,3 +1,6 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_control
+from django.views.decorators.gzip import gzip_page
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from recipes.api.filters import SearchVectorFilter
@@ -5,6 +8,8 @@ from recipes.api.serializers import CategorySerializer, RecipeSerializer, Cuisin
 from recipes.models import Category, Recipe, Cuisine
 
 
+@method_decorator(gzip_page, name='dispatch')
+@method_decorator(cache_control(max_age=3600), name='dispatch')
 class CuisineViewSet(viewsets.ModelViewSet):
     queryset = Cuisine.objects.all()
     serializer_class = CuisineSerializer
@@ -12,6 +17,8 @@ class CuisineViewSet(viewsets.ModelViewSet):
     search_fields = ['name']
 
 
+@method_decorator(gzip_page, name='dispatch')
+@method_decorator(cache_control(max_age=3600), name='dispatch')
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
@@ -19,6 +26,8 @@ class CategoryViewSet(viewsets.ModelViewSet):
     search_fields = ['name']
 
 
+@method_decorator(gzip_page, name='dispatch')
+@method_decorator(cache_control(max_age=3600), name='dispatch')
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
