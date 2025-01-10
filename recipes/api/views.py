7e7ea4090cfad6a30273ab1ba56ccb3d8eb19d6c
danny <flackattack@gmail.com>
2025-01-10 15:47:20 -0600

    just the recipe: api endpoint

---
+++ recipes/api/views.py
@@ -0,0 +1,31 @@
+import requests
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.permissions import AllowAny
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from recipe_scrapers import scrape_html
+
+
+CACHE_MINUTE = 60
+CACHE_HOUR = CACHE_MINUTE * 60
+CACHE_HALF_DAY = CACHE_HOUR * 12
+CACHE_DAY = CACHE_HALF_DAY * 2
+
+
+class JustTheRecipeView(APIView):
+    permission_classes = (AllowAny,)
+
+    @method_decorator(cache_page(CACHE_DAY))
+    def get(self, request):
+        if 'url' not in request.GET:
+            raise ValidationError({'url': 'Missing "url" parameter'})
+        url = request.GET['url']
+        # retrieve the recipe webpage HTML
+        req = requests.get(url)
+        req.raise_for_status()
+        html = req.content
+        # pass the html alongside the url to our scrape_html function
+        scraper = scrape_html(html, org_url=url, wild_mode=True)
+        return Response({'recipe': scraper.to_json()})
