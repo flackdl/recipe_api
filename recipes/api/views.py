83ea423444810e3a01ea79b1090fef7b43d6746e
danny <flackattack@gmail.com>
2025-11-02 07:41:48 -0600

    add browser headers

--- recipes/api/views.py
+++ recipes/api/views.py
@@ -20,6 +20,16 @@ CACHE_DAY = CACHE_HALF_DAY * 2
 class JustTheRecipeView(APIView):
     permission_classes = (AllowAny,)
 
+    # try and mimic a browser
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+    }
+
     @method_decorator(cache_page(CACHE_DAY))
     def get(self, request):
         # validate url exists
@@ -30,7 +40,7 @@ class JustTheRecipeView(APIView):
         if not url.startswith('http'):
             raise ValidationError({'url': "Invalid 'url' parameter"})
         # retrieve the recipe webpage HTML
-        req = requests.get(url)
+        req = requests.get(url, headers=self.headers, timeout=20)
         if req.status_code < 200 or req.status_code >= 300:
             raise ValidationError({'url': 'URL returned an error'})
         html = req.content
